8ce3e94ee70eca1b31d1c78aa5f5b89eb0050b60
Bharath H J <41682550+bharath-hj238@users.noreply.github.com>
2021-08-23 11:11:00 +0530

    Print samples with f-string usage

--- 001_print.py
+++ 001_print.py
@@ -2,18 +2,23 @@
 num_variable = 23
 string_variable = "name_of_member"
 
+
 # Print a variable
 print (num_variable, string_variable)
 
+
 # Print a text
 print ("Hello..")
+
 
 # Print types of variables
 print ("Number is ", str(num_variable))  # Integer
 print ("Text is ", string_variable) # String
 
+
 # Print test along with variable (interger and string) - using f-string
 print (f"Hello.. {num_variable}")
+
 
 # Concatenate two strings
 print ("Hello", " ", "there!")
