afbcdaeeb74edf1ac7a180b9c28700cb3b8ff7b5
Bharath H J <41682550+bharath-hj238@users.noreply.github.com>
2021-08-25 23:26:25 +0530

    Play Rock-Paper-Scissor
    Welcome to Rock-Paper-Scissor
    
    
    Choose an option: Rock[1] Paper[2] Scissor[3]: 2
    
         _______      
    ---'    ____)____ 
               ______)
              _______)
             _______) 
    ---.__________)
    
    
     Computer Chose:
    
        _______
    ---'   ____)____
              ______)
           __________)
          (____)
    '----(___)
    
    
    
    You Lose

--- scripts/script_rock_paper_scissor.py
+++ scripts/script_rock_paper_scissor.py
@@ -29,16 +29,15 @@ decision = [
 
 decision_list = [0, 1, 2]
 print("\nWelcome to Rock-Paper-Scissor\n")
-
 user_choice = int(input("\nChoose an option: Rock[1] Paper[2] Scissor[3]: ")) - 1
-
 print(decision[user_choice])
-
 computer_choice = int(random.choice(decision_list))
+
 
 print("\n Computer Chose: ")
 print(decision[computer_choice])
 print("\n")
+
 
 if user_choice > 3:
     print("You Lose")
